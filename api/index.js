328136587c6fe0e12b5a041c94da92e385de08b8
Abinand-hub <abinand720@gmail.com>
2026-09-18 18:02:38 +0530

    Configure CommonJS api runtime with api/package.json and api/server_bundle.js for Vercel

--- api/index.js
+++ api/index.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const bundle = require(path.join(__dirname, 'server_bundle.cjs'));
+const bundle = require(path.join(__dirname, 'server_bundle.js'));
 const app = bundle.app || bundle.default || bundle;
 
 module.exports = function handler(req, res) {
